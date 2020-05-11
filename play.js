827669229c6583cf546574b5ee7835ba65a565ec
Stefan Braunovic <Stefan.braunovic55@gmail.com>
2020-05-11 14:44:09 +0200

    fix2

--- play.js
+++ play.js
@@ -4,6 +4,7 @@ function playSound(e){
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     
     if(!audio) return; 
+    
     audio.currentTime=0;
     audio.play()
     key.classList.add('playing');
